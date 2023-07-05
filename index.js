587f0f8d76da24315e7c7d42f234055d9d01fefa
Iddan Aaronsohn <mail@aniddan.com>
2023-07-05 22:41:47 +0200

    Add check chrome is not running and make code more linear

--- index.js
+++ index.js
@@ -5,25 +5,18 @@ import { spawn } from "child_process";
 // regex for DevTools listening on ws://127.0.0.1:9222/devtools/browser/64e21d33-4771-4a66-9053-426b594253ce
 const REGEX = /DevTools listening on (ws:\/\/.*)/;
 
-const chromeProcess = spawn(
-  "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --remote-debugging-port=9222",
-  {
-    shell: true,
-  }
-);
-
-chromeProcess.stderr.on("data", (data) => {
-  console.log(data.toString());
-  const match = REGEX.exec(data.toString());
-  if (match) {
-    main(match[1]).catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-  }
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
 });
 
-async function main(address) {
+async function main() {
+  const chromeProcesses = await pgrepChrome();
+  if (chromeProcesses.length > 0) {
+    console.log("Chrome is already running, please close it and try again");
+    process.exit(1);
+  }
+  const { chromeProcess, address } = await spawnChromeDevTools();
   const browser = await puppeteer.connect({
     browserWSEndpoint: address,
   });
@@ -37,29 +30,33 @@ async function main(address) {
   for (const [url, description] of Object.entries(locations)) {
     current++;
     console.log(`[${current}/${total}] ${url}`);
-    await page.goto(url);
-    try {
-      await clickByText(page, "Save");
-    } catch (error) {
-      console.warn("Couldn't save, skipping");
-      summary.cantSave.push(url);
-      continue;
-    }
-    await clickByText(page, "France w Mom 23");
-    try {
-      await clickByText(page, "Add note");
-      await page.waitForSelector("textarea");
-      await page.type("textarea", description);
-      await clickByText(page, "Done");
-    } catch (error) {
-      console.warn("Couldn't add note, skipping");
-      summary.cantAddNote.push(url);
-    }
+    await handleLocation(url, description, page, summary);
     await sleep(100);
   }
   console.log("Summary:");
   console.log(summary);
   await browser.close();
+}
+
+async function handleLocation(url, description, page, summary) {
+  await page.goto(url);
+  try {
+    await clickByText(page, "Save");
+  } catch (error) {
+    console.warn("Couldn't save, skipping");
+    summary.cantSave.push(url);
+    return;
+  }
+  await clickByText(page, "France w Mom 23");
+  try {
+    await clickByText(page, "Add note");
+    await page.waitForSelector("textarea");
+    await page.type("textarea", description);
+    await clickByText(page, "Done");
+  } catch (error) {
+    console.warn("Couldn't add note, skipping");
+    summary.cantAddNote.push(url);
+  }
 }
 
 function sleep(timeout) {
@@ -88,4 +85,36 @@ async function clickByText(page, text, timeout = 200) {
     throw new Error(`Element with text "${text}" not found`);
   }
   return await element.click();
+}
+
+async function pgrepChrome() {
+  return new Promise((resolve, reject) => {
+    const process = spawn("pgrep", ["-f", "Google Chrome"]);
+    const output = [];
+    process.stdout.on("data", (data) => {
+      output.push(data.toString());
+    });
+    process.on("close", (code) => {
+      resolve(output);
+    });
+  });
+}
+
+function spawnChromeDevTools() {
+  return new Promise((resolve, reject) => {
+    const chromeProcess = spawn(
+      "/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --remote-debugging-port=9222",
+      {
+        shell: true,
+      }
+    );
+
+    chromeProcess.stderr.on("data", (data) => {
+      console.log(data.toString());
+      const match = REGEX.exec(data.toString());
+      if (match) {
+        resolve({ chromeProcess, address: match[1] });
+      }
+    });
+  });
 }
